fb80daf96644500c765563745cc2297f2068a473
René Hansen <rene@hansen-audio.de>
2021-03-05 21:53:25 +0100

    Rename Real to float_t

--- include/ha/dsp_tool_box/filtering/one_pole.h
+++ include/ha/dsp_tool_box/filtering/one_pole.h
@@ -17,25 +17,25 @@ public:
     //--------------------------------------------------------------------
     struct context_data
     {
-        Real a = 0;
-        Real b = 0;
-        Real z = 0;
+        float_t a = 0;
+        float_t b = 0;
+        float_t z = 0;
     };
 
-    static context_data init(Real a = 0.9)
+    static context_data init(float_t a = 0.9)
     {
-        context_data data{a, Real(1.) - a, 0};
+        context_data data{a, float_t(1.) - a, 0};
 
         return data;
     }
 
-    static void update_pole(Real a, context_data& data)
+    static void update_pole(float_t a, context_data& data)
     {
         data.a = a;
-        data.b = Real(1.) - data.a;
+        data.b = float_t(1.) - data.a;
     }
 
-    static Real smooth(Real in, context_data& data)
+    static float_t smooth(float_t in, context_data& data)
     {
         if (is_equal(in, data))
             return data.z;
@@ -44,18 +44,18 @@ public:
         return data.z;
     }
 
-    static void reset(Real in, context_data& data) { data.z = in; }
+    static void reset(float_t in, context_data& data) { data.z = in; }
 
-    static Real tau_to_pole(Real tau, Real sample_rate)
+    static float_t tau_to_pole(float_t tau, float_t sample_rate)
     {
         //! https://en.wikipedia.org/wiki/Time_constant
         //! (5 * Tau) means 99.3% reached thats sufficient.
-        constexpr Real RECIPROCAL_5 = Real(1. / 5.);
-        return exp(Real(-1) / ((tau * RECIPROCAL_5) * sample_rate));
+        constexpr float_t RECIPROCAL_5 = float_t(1. / 5.);
+        return exp(float_t(-1) / ((tau * RECIPROCAL_5) * sample_rate));
     }
     //--------------------------------------------------------------------
 private:
-    static bool is_equal(Real in, const context_data& data) { return in == data.z; }
+    static bool is_equal(float_t in, const context_data& data) { return in == data.z; }
 };
 
 //-----------------------------------------------------------------------------

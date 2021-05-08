382e37aa7d27947bd98cfa43c72d86ef419240da
René Hansen <rene@hansen-audio.de>
2021-05-08 20:07:27 +0200

    Add documentation to phase. Remove adsr files.

--- include/ha/dsp_tool_box/modulation/modulation_phase.h
+++ include/ha/dsp_tool_box/modulation/modulation_phase.h
@@ -10,12 +10,14 @@ namespace ha {
 namespace dtb {
 namespace modulation {
 
-//------------------------------------------------------------------------
 /**
- * phase
+ * @brief Phase running from 0 to 1 and can be used for e.g. an LFO
  */
 struct phase
 {
+    /**
+     * @brief Sync mode of the phase
+     */
     enum class sync_mode
     {
         FREE = 0,
@@ -23,6 +25,9 @@ struct phase
         PROJECT_SYNC
     };
 
+    /**
+     * @brief Context of a phase
+     */
     struct context
     {
         mut_real rate                = real(0.);
@@ -34,15 +39,72 @@ struct phase
         mut_real tempo_synced_factor = real(0.);
     };
 
+    /**
+     * @brief Advances the phase value according to the given context
+     *
+     * @param value Current phase value
+     * @param num_samples Number of samples to advance
+     * @return Returns true once overflown
+     */
     static bool
     advance_one_shot(context const& cx, mut_real& value, i32 num_samples);
+
+    /**
+     * @brief Advances the phase value according to the given context
+     *
+     * @param value Current phase value
+     * @param num_samples Number of samples to advance
+     * @return Returns true on overflow
+     */
     static bool advance(context const& cx, mut_real& value, i32 num_samples);
+
+    /**
+     * @brief Sets the sync mode of context
+     *
+     * @param value \sa sync_mode
+     */
     static void set_sync_mode(context& cx, sync_mode value);
+
+    /**
+     * @brief Sets the tempo of context
+     *
+     * @param value Tempo in [bpm], e.g. 120
+     */
     static void set_tempo(context& cx, real value);
+
+    /**
+     * @brief Sets the rate of context
+     *
+     * @param value \sa note_length_to_rate
+     */
     static void set_rate(context& cx, real value);
+
+    /**
+     * @brief Sets the sample rate of context
+     *
+     * @param value Sample rate in [Hz]
+     */
     static void set_sample_rate(context& cx, real value);
+
+    /**
+     * @brief Sets the project time of context
+     *
+     * @param value Project time in musical beats
+     */
     static void set_project_time(context& cx, real value);
+
+    /**
+     * @brief Sets the note len of context
+     *
+     * @param value Note len e.g. 1/32 -> 0.03125
+     */
     static void set_note_length(context& cx, real value);
+
+    /**
+     * @brief Converts note len to rate (call set_rate(...) afterwards)
+     *
+     * @param value value Note len e.g. 1/32 -> 0.03125
+     */
     static real note_length_to_rate(real value);
 };
 

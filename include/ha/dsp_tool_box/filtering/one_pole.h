562b8cda450b23abadf45cb7baa038cbc9fe3c96
René Hansen <rene@hansen-audio.de>
2021-05-11 19:18:43 +0200

    Move implementations to source file

--- include/ha/dsp_tool_box/filtering/one_pole.h
+++ include/ha/dsp_tool_box/filtering/one_pole.h
@@ -13,10 +13,8 @@ namespace filtering {
  * @brief A one-pole filter for e.g. parameter smoothing.
  *
  */
-class one_pole_filter final
+struct one_pole_filter final
 {
-public:
-    //-------------------------------------------------------------------------
     struct context
     {
         mut_real a = 0;
@@ -24,40 +22,11 @@ public:
         mut_real z = 0;
     };
 
-    static context create(real a = 0.9)
-    {
-        context cx{a, real(1.) - a, 0};
-
-        return cx;
-    }
-
-    static void update_pole(context& cx, real a)
-    {
-        cx.a = a;
-        cx.b = real(1.) - cx.a;
-    }
-
-    static real process(context& cx, real in)
-    {
-        if (is_equal(cx, in))
-            return cx.z;
-
-        cx.z = (in * cx.b) + (cx.z * cx.a);
-        return cx.z;
-    }
-
-    static void reset(context& cx, real in) { cx.z = in; }
-
-    static real tau_to_pole(real tau, real sample_rate)
-    {
-        //! https://en.wikipedia.org/wiki/Time_constant
-        //! (5 * Tau) means 99.3% reached thats sufficient.
-        constexpr real RECIPROCAL_5 = real(1. / 5.);
-        return exp(real(-1) / ((tau * RECIPROCAL_5) * sample_rate));
-    }
-    //-------------------------------------------------------------------------
-private:
-    static bool is_equal(const context& cx, real in) { return in == cx.z; }
+    static context create(real a = 0.9);
+    static void update_pole(context& cx, real a);
+    static real process(context& cx, real in);
+    static void reset(context& cx, real in);
+    static real tau_to_pole(real tau, real sample_rate);
 };
 
 //-----------------------------------------------------------------------------
---
+++ source/filtering/one_pole.cpp
@@ -0,0 +1,64 @@
+// Copyright(c) 2021 Hansen Audio.
+
+#include "ha/dsp_tool_box/filtering/one_pole.h"
+
+#include <math.h>
+
+namespace ha {
+namespace dtb {
+namespace filtering {
+namespace {
+
+//-----------------------------------------------------------------------------
+bool is_equal(const one_pole_filter::context& cx, real in)
+{
+    return in == cx.z;
+}
+
+//-----------------------------------------------------------------------------
+} // namespace
+
+//-----------------------------------------------------------------------------
+one_pole_filter::context one_pole_filter::create(real a)
+{
+    context cx{a, real(1.) - a, 0};
+
+    return cx;
+}
+
+//-----------------------------------------------------------------------------
+void one_pole_filter::update_pole(context& cx, real a)
+{
+    cx.a = a;
+    cx.b = real(1.) - cx.a;
+}
+
+//-----------------------------------------------------------------------------
+real one_pole_filter::process(context& cx, real in)
+{
+    if (is_equal(cx, in))
+        return cx.z;
+
+    cx.z = (in * cx.b) + (cx.z * cx.a);
+    return cx.z;
+}
+
+//-----------------------------------------------------------------------------
+void one_pole_filter::reset(context& cx, real in)
+{
+    cx.z = in;
+}
+
+//-----------------------------------------------------------------------------
+real one_pole_filter::tau_to_pole(real tau, real sample_rate)
+{
+    //! https://en.wikipedia.org/wiki/Time_constant
+    //! (5 * Tau) means 99.3% reached thats sufficient.
+    constexpr real RECIPROCAL_5 = real(1. / 5.);
+    return exp(real(-1) / ((tau * RECIPROCAL_5) * sample_rate));
+}
+
+//-----------------------------------------------------------------------------
+} // namespace filtering
+} // namespace dtb
+} // namespace ha
